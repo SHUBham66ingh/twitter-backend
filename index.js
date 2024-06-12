beaeca935fcd74649ddbe9ba728b6efb5f667b30
Shubham singh <shubham1041singh@gmail.com>
2024-06-13 01:38:15 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express= require('express');
+const app = express()
+app.get('/' , (req,res)=>{
+    res.send("hello shubham singh")
+})
+
+app.get('/user' , (req,res)=>{
+    res.send("hello user how are you")
+})
+
+app.post('/' , (req,res)=>{
+    res.send("got a post request")
+})
+
+
+ const PORT=3000;
+
+app.listen( PORT   , ()=>{
+    console.log(`server connected at port ${PORT}`);
+})
